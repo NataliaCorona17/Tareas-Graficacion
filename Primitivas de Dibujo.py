034caff2cd3e812ad45fde98543c8c1ca90f4eb9
NataliaCorona17 <nataliagcc17gmail.com>
2025-02-19 10:19:51 -0600

    Desplazamiento

---
+++ Primitivas de Dibujo.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import numpy as np
+
+img = np.ones ((500, 500, 3), dtype=np.uint8) * 255 
+
+for i in range (400):
+    img = np.ones ((500, 500, 3), dtype = np.uint8) * 255
+    cv.circle (img, (0+i, 0+i), 20, (0, 234, 21), -1)
+    cv.imshow ('img', img)
+    cv.waitKey (40)
+
+
+cv.waitKey()
+cv.destroyAllWindows()
